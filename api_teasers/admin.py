8811d9b80efcf253dc73f675d26f7e169b9a7001
Gaidunov Vasiliy <gaidunov18@gmail.com>
2023-02-05 20:41:49 +0400

    Схема бд, модели + админка

--- api_teasers/admin.py
+++ api_teasers/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Teaser, Wallet
 
+@admin.register(Teaser)
+class TeaserAdmin(admin.ModelAdmin):
+    pass 
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    pass 
 # Register your models here.
--- api_teasers/models.py
+++ api_teasers/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    # Дополнительные поля модели User, если необходимы
+    pass
+
+class Teaser(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    category = models.CharField(max_length=64)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=64, choices=(("pending", "pending"), ("approved", "approved"), ("declined", "declined")))
+
+    def __str__(self):
+        return self.title
+
+class Wallet(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.FloatField()
+
+    def __str__(self):
+        return f"{self.user} Wallet"
